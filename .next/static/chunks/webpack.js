8431e699870bbc2e08874d993d5f490105fec020
Michael Preciado <mpreciado1997@gmail.com>
2025-06-01 02:56:42 -0700

    Update build artifacts and cache files; modify app-paths manifest for routing consistency and update webpack hash for client-side assets.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7e990747f51257ec"; }
+/******/ 		__webpack_require__.h = function() { return "2bbe7df049523fa8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
