8a11f77f337a21c635a1e06ce8fa80743386dbf8
Michael Preciado <mpreciado1997@gmail.com>
2025-06-24 13:24:59 -0700

    style: update ESLint configuration to support TypeScript
    - Added TypeScript ESLint plugin and parser to enhance linting capabilities for TypeScript files.
    - Extended ESLint configuration to include recommended rules for TypeScript, improving code quality and consistency.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   extends: [
     'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
   ],
   root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     // Add custom rules here if needed
   },
